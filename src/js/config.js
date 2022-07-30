216f262369228065f51238a61cc6ec657931c120
MostafaHamdy3 <73959716+MostafaHamdy3@users.noreply.github.com>
2022-07-30 05:06:39 +0200

    nitial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = `https://forkify-api.herokuapp.com/api/v2/recipes`;
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = "3ebba09c-c828-4e11-813b-565b6c39faee";
+export const MODAL_CLOSE_SEC = 3;
